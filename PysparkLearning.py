0fc77de7df43f109c5a551e13d314c50918ebd37
pkapusta <piotrkap97@gmail.com>
2023-07-31 20:59:25 +0200

    small changes

--- PysparkLearning.py
+++ PysparkLearning.py
@@ -1,1 +1,12 @@
 from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Spark Learning").getOrCreate()
+
+df = spark.read.format('com.databricks.spark.csv').\
+    options(header=True,
+    inferschema='true').\
+    load('/home/piotrek/Projects/RF_Trading/datafile/BNBBUSD.csv')
+        
+    
+df.show(5)
+df.printSchema()
