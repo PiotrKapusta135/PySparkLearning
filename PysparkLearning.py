c954563173fadae5db304cef9646f96a8e216c86
pkapusta <piotrkap97@gmail.com>
2023-10-01 23:36:17 +0200

    page 120

--- PysparkLearning.py
+++ PysparkLearning.py
@@ -371,4 +371,49 @@ df_miss_no_income = df_miss.select([ c for c in df_miss.columns if c!= 'income']
 
 df_miss_no_income.show()
 
-# df_miss_no_income.dropna(thresh=3).show()
+df_miss_no_income.dropna(thresh=3).show()
+
+means = df_miss_no_income.agg(
+    *[fn.mean(c).alias(c)
+      for c in df_miss_no_income.columns if c != 'gender'])\
+    .toPandas().to_dict('records')[0]
+    
+means['gender'] = 'missing'
+
+df_miss_no_income.fillna(means).show()
+
+df_outliers = spark.createDataFrame([
+(1, 143.5, 5.3, 28),
+(2, 154.2, 5.5, 45),
+(3, 342.3, 5.1, 99),
+(4, 144.5, 5.5, 33),
+(5, 133.2, 5.4, 54),
+(6, 124.1, 5.1, 21),
+(7, 129.2, 5.3, 42),
+], ['id', 'weight', 'height', 'age'])
+
+cols = ['weight' , 'height', 'age']
+bounds = {}
+
+for col in cols:
+    quantiles = df_outliers.approxQuantile(col, [0.25, 0.75], 0.05)
+    IQR = quantiles[1] - quantiles[0]
+    
+    bounds[col] = [
+        quantiles[0] - 1.5 * IQR,
+        quantiles[1] + 1.5 * IQR
+        ]
+
+outliers = df_outliers.select(*['id'] + [
+    (
+     (df_outliers[c] < bounds[c][0])|
+     (df_outliers[c] > bounds[c][1])
+     ).alias(c + '_o') for c in cols
+    ])
+
+outliers.show()
+
+df_outliers = df_outliers.join(outliers, on='id')
+df_outliers.show()
+df_outliers.filter('weight_o').select('id', 'weight').show()
+df_outliers.filter('age_o').select('id', 'age').show()
